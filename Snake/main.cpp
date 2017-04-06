acd7ed0cd2304768a151c94121f6469e08bcb129
ZurekTomasz <a@a.pl>
2017-04-07 01:58:19 +0200

    0.0.2
    Add SFML and graphics

--- Snake/main.cpp
+++ Snake/main.cpp
@@ -1,102 +1,171 @@
 #include "stdafx.h"
 
-int lwierszy = 24;
-int lkolumn = 78;
-char mapa[24][78];
-int snake_x[5];
+sf::RenderWindow renderWindow(sf::VideoMode(1920, 1080), "SpaceShips");
+sf::Event event;
+
+void keyboard();
+
+int lwierszy = 96;
+int lkolumn = 54;
+char mapa[96][54];
+int snake_x[15];
 int snake_y[5];
+int szer_klocka = 20;
 
 int main()
 {
-	printf("Snake Game 2D in console");
-	_getch();
+	
 
-	snake_x[0] = 20;
-	snake_y[0] = 20;
-	snake_x[1] = 20;
-	snake_y[1] = 21;
-	snake_x[2] = 20;
-	snake_y[2] = 22;
-	snake_x[3] = 20;
-	snake_y[3] = 23;
-	char key;
-	key = 'a';
-	while (key != '.') 
+	//Inicjacja//
+	renderWindow.setFramerateLimit(60);
+
+	//Ladowanie tekstur
+	sf::Texture t_tlo;
+	if (!t_tlo.loadFromFile("grafika/tlo.png"))
+		return EXIT_FAILURE;
+	sf::Sprite tlo;
+	tlo.setTexture(t_tlo);
+
+	sf::Texture t_trawa;
+	if (!t_trawa.loadFromFile("grafika/trawa.png"))
+		return EXIT_FAILURE;
+	sf::Sprite trawa;
+	trawa.setTexture(t_trawa);
+
+	sf::Texture t_niebieskie;
+	if (!t_niebieskie.loadFromFile("grafika/niebieskie.png"))
+		return EXIT_FAILURE;
+	sf::Sprite niebieskie;
+	niebieskie.setTexture(t_niebieskie);
+
+	sf::Texture t_czerwone;
+	if (!t_czerwone.loadFromFile("grafika/czerwone.png"))
+		return EXIT_FAILURE;
+	sf::Sprite czerwone;
+	czerwone.setTexture(t_czerwone);
+	//Inicjacja//
+
+	snake_x[0] = 10;
+	snake_y[0] = 10;
+	snake_x[1] = 10;
+	snake_y[1] = 11;
+	snake_x[2] = 10;
+	snake_y[2] = 12;
+	snake_x[3] = 10;
+	snake_y[3] = 13;
+
+	mapa[snake_x[0]][snake_y[0]] = '1';
+	mapa[snake_x[1]][snake_y[1]] = '1';
+	mapa[snake_x[2]][snake_y[2]] = '1';
+	mapa[snake_x[3]][snake_y[3]] = '1';
+
+
+	while (renderWindow.isOpen())
 	{
+		keyboard();
 
-		for (int a = 0; a < lwierszy; a++)
+		renderWindow.clear();
+		for (int a = 0; a < 96; a++)
 		{
-			for (int b = 0; b < lkolumn; b++)
+			for (int b = 0; b < 54; b++)
 			{
-				mapa[a][b] = ' ';
+				mapa[a][b] = '0';
 			}
 		}
-
-		mapa[snake_x[0]][snake_y[0]] = '1';
+		
+		mapa[snake_x[0]][snake_y[0]] = '2';
 		mapa[snake_x[1]][snake_y[1]] = '2';
-		mapa[snake_x[2]][snake_y[2]] = '3';
-		mapa[snake_x[3]][snake_y[3]] = '4';
-		mapa[snake_x[4]][snake_y[4]] = ' ';
+		mapa[snake_x[2]][snake_y[2]] = '2';
+		mapa[snake_x[3]][snake_y[3]] = '1';
+		
 
-		for (int a = 0; a < lwierszy; a++)
+		for (int a = 0; a < 96; a++)
 		{
-			for (int b = 0; b < lkolumn; b++)
+			for (int b = 0; b < 54; b++)
 			{
-				cout << mapa[a][b];
+				if (mapa[a][b] == '0')
+				{
+					trawa.setPosition(a * szer_klocka, b * szer_klocka);
+					renderWindow.draw(trawa);
+				}
+
+				if (mapa[a][b] == '1')
+				{
+					czerwone.setPosition(a * szer_klocka, b * szer_klocka);
+					renderWindow.draw(czerwone);
+				}
+
+				if (mapa[a][b] == '2')
+				{
+					niebieskie.setPosition(a * szer_klocka, b * szer_klocka);
+					renderWindow.draw(niebieskie);
+				}
 			}
 			printf("\n");
 		}
 
-		cin >> key;
-		if (key == 'w')
-		{
-			snake_x[0] = snake_x[1];
-			snake_y[0] = snake_y[1];
-			snake_x[1] = snake_x[2];
-			snake_y[1] = snake_y[2];
-			snake_x[2] = snake_x[3];
-			snake_y[2] = snake_y[3];
-			snake_x[3] -= 1;
-			snake_y[3] += 0;
-		}
-
-		if (key == 'a')
-		{
-			snake_x[0] = snake_x[1];
-			snake_y[0] = snake_y[1];
-			snake_x[1] = snake_x[2];
-			snake_y[1] = snake_y[2];
-			snake_x[2] = snake_x[3];
-			snake_y[2] = snake_y[3];
-			snake_x[3] += 0;
-			snake_y[3] -= 1;
-		}
-
-		if (key == 's')
-		{
-			snake_x[0] = snake_x[1];
-			snake_y[0] = snake_y[1];
-			snake_x[1] = snake_x[2];
-			snake_y[1] = snake_y[2];
-			snake_x[2] = snake_x[3];
-			snake_y[2] = snake_y[3];
-			snake_x[3] += 1;
-			snake_y[3] += 0;
-		}
-
-		if (key == 'd')
-		{
-			snake_x[0] = snake_x[1];
-			snake_y[0] = snake_y[1];
-			snake_x[1] = snake_x[2];
-			snake_y[1] = snake_y[2];
-			snake_x[2] = snake_x[3];
-			snake_y[2] = snake_y[3];
-			snake_x[3] += 0;
-			snake_y[3] += 1;
-		}
-
+		renderWindow.display();
 	}
+
 	_getch();
 	return 0;
+}
+
+void keyboard()
+{
+	while (renderWindow.pollEvent(event)) {
+		if (event.type == sf::Event::EventType::Closed)
+			renderWindow.close();
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+			renderWindow.close();
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+	{
+		snake_x[0] = snake_x[1];
+		snake_y[0] = snake_y[1];
+		snake_x[1] = snake_x[2];
+		snake_y[1] = snake_y[2];
+		snake_x[2] = snake_x[3];
+		snake_y[2] = snake_y[3];
+		snake_x[3] -= 1;
+		snake_y[3] += 0;
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+	{
+		snake_x[0] = snake_x[1];
+		snake_y[0] = snake_y[1];
+		snake_x[1] = snake_x[2];
+		snake_y[1] = snake_y[2];
+		snake_x[2] = snake_x[3];
+		snake_y[2] = snake_y[3];
+		snake_x[3] += 0;
+		snake_y[3] -= 1;
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+	{
+		snake_x[0] = snake_x[1];
+		snake_y[0] = snake_y[1];
+		snake_x[1] = snake_x[2];
+		snake_y[1] = snake_y[2];
+		snake_x[2] = snake_x[3];
+		snake_y[2] = snake_y[3];
+		snake_x[3] += 1;
+		snake_y[3] += 0;
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+	{
+		snake_x[0] = snake_x[1];
+		snake_y[0] = snake_y[1];
+		snake_x[1] = snake_x[2];
+		snake_y[1] = snake_y[2];
+		snake_x[2] = snake_x[3];
+		snake_y[2] = snake_y[3];
+		snake_x[3] += 0;
+		snake_y[3] += 1;
+	}
 }
--- Snake/stdafx.h
+++ Snake/stdafx.h
@@ -5,5 +5,16 @@
 #include <iostream>
 #include <cstdio>
 #include <conio.h>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <windows.h>
+#include <cstdlib>
+#include <chrono>
+#include <string>
+
+using namespace std;
+
+
 
 using namespace std;
